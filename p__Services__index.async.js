22e22a6e42b6c81fad1d995455d7fde4e5691dd7
Zhuzrx <zhouruixingzhu@link.cuhk.edu.cn>
2024-03-11 01:28:39 +0800

    Updates

--- p__Services__index.async.js
+++ p__Services__index.async.js
@@ -1,32 +1,11 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[767],{65273:function(l,r,n){n.d(r,{Z:function(){return g}});var e=n(62435),t=n(46837),o=n(8250),c=n(29671),a=n(20640),d=n.n(a),h=n(33862),v=n(1210),m=n(85093),x=n(10524),u=n(14079),i=n(49721),j=n.p+"static/logo.932a0d69.png",f=`**Mphil Student**   \r
-in CUHKSZ.  \r
-Interested in **LLM**.`,s=n(86074),g=function(){var N=function(S){S.preventDefault(),d()(i.email),t.ZP.success("Email copied to clipboard",1)};return(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(o.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:j}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:i.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(c.U,{children:f})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(h.Z,{className:"icon"}),i.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(v.Z,{className:"icon"}),(0,s.jsx)("a",{href:i.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(m.Z,{className:"icon"}),(0,s.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(u.Z,{className:"icon"}),(0,s.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},92244:function(l,r,n){n.r(r),n.d(r,{default:function(){return d}});var e=n(62435),t=`# \u6807\u9898 \u5927\u5BB6\u597D\r
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[767],{65273:function(o,a,n){n.d(a,{Z:function(){return g}});var d=n(62435),l=n(46837),r=n(8250),t=n(29671),e=n(20640),c=n.n(e),h=n(33862),v=n(1210),m=n(85093),u=n(10524),x=n(14079),s=n(49721),f=n.p+"static/logo.4700deb7.png",j="Mphil student in CUHKSZ.  ",i=n(86074),g=function(){var S=function(E){E.preventDefault(),c()(s.email),l.ZP.success("Email copied to clipboard",1)};return(0,i.jsxs)("div",{className:"sidebar",children:[(0,i.jsxs)("div",{className:"profile",children:[(0,i.jsx)(r.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:f}),(0,i.jsxs)("div",{className:"info",children:[(0,i.jsx)("div",{className:"name",children:s.name}),(0,i.jsx)("div",{className:"shortInfo",children:(0,i.jsx)(t.U,{children:j})})]})]}),(0,i.jsxs)("div",{className:"link",children:[(0,i.jsxs)("div",{children:[(0,i.jsx)(h.Z,{className:"icon"}),s.location]}),(0,i.jsxs)("div",{children:[(0,i.jsx)(v.Z,{className:"icon"}),(0,i.jsx)("a",{href:s.github,children:"Github"})]}),(0,i.jsxs)("div",{children:[(0,i.jsx)(m.Z,{className:"icon"}),(0,i.jsx)("a",{href:s==null?void 0:s.twitter,children:"Twitter"})]}),(0,i.jsxs)("div",{children:[(0,i.jsx)(u.Z,{className:"icon"}),(0,i.jsx)("a",{onClick:S,href:"",children:"Email"})]}),(0,i.jsxs)("div",{children:[(0,i.jsx)(x.Z,{className:"icon"}),(0,i.jsx)("a",{href:s==null?void 0:s.googleScholar,children:"Google Scholar"})]})]})]})}},92244:function(o,a,n){n.r(a),n.d(a,{default:function(){return c}});var d=n(62435),l=`# Service\r
 \r
-\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002\r
+## 2024\r
 \r
-## \u5C0F\u6807\u9898 1\r
+- External Reviewer \u2013 46th International Conference on Software Engineering (ICSE 2024)\r
+- External Reviewer \u2013 33rd ACM International Conference on the Foundations of Software Engineering (FSE 2024)\r
 \r
-- \u5217\u8868\u98791\r
-- \u5217\u8868\u98792\r
-- \u5217\u8868\u98793\r
-\r
-### \u5C0F\u5C0F\u6807\u9898 1\r
-\r
-\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002\r
-\r
-#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
-\r
-1. \u6709\u5E8F\u5217\u8868\u98791\r
-2. \u6709\u5E8F\u5217\u8868\u98792\r
-3. \u6709\u5E8F\u5217\u8868\u98793\r
-\r
-##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
-\r
-> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002\r
-\r
-###### \u6700\u5C0F\u6807\u9898\r
-\r
-\`\`\`python\r
-# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801\r
-print("Hello, Markdown!")\r
-`,o=n(65273),c=n(29671),a=n(86074),d=function(){return(0,a.jsxs)("div",{className:"page",children:[(0,a.jsx)(o.Z,{}),(0,a.jsxs)("div",{className:"content",children:[(0,a.jsx)("div",{className:"title",children:"Services"}),(0,a.jsx)("div",{className:"markdown",children:(0,a.jsx)(c.U,{children:t})})]})]})}},49721:function(l){l.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?user=3z3z3z4AAAAJ&hl=en","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
+### 2023\r
+- External Reviewer \u2013 32nd ACM Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering (ESEC/FSE 2023)\r
+- External Reviewer \u2013 34th IEEE International Symposium on Software Reliability Engineering (ISSRE 2023)\r
+`,r=n(65273),t=n(29671),e=n(86074),c=function(){return(0,e.jsxs)("div",{className:"page",children:[(0,e.jsx)(r.Z,{}),(0,e.jsxs)("div",{className:"content",children:[(0,e.jsx)("div",{className:"title",children:"Services"}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsx)(t.U,{children:l})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/shuaijiumei","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","publications","services","cv"]}')}}]);
