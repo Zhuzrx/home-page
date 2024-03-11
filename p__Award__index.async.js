94da02017b00449c00ed388c51b0252eaf7866b7
Zhuzrx <zhouruixingzhu@link.cuhk.edu.cn>
2024-03-11 14:09:27 +0800

    Updates

--- p__Award__index.async.js
+++ p__Award__index.async.js
@@ -1,1 +1,35 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[317],{65273:function(o,l,s){s.d(l,{Z:function(){return g}});var h=s(62435),d=s(46837),c=s(8250),t=s(29671),i=s(20640),e=s.n(i),r=s(33862),v=s(1210),m=s(85093),x=s(10524),u=s(14079),a=s(49721),j=s.p+"static/logo.4700deb7.png",f="Mphil student in CUHKSZ.  ",n=s(86074),g=function(){var N=function(Z){Z.preventDefault(),e()(a.email),d.ZP.success("Email copied to clipboard",1)};return(0,n.jsxs)("div",{className:"sidebar",children:[(0,n.jsxs)("div",{className:"profile",children:[(0,n.jsx)(c.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:j}),(0,n.jsxs)("div",{className:"info",children:[(0,n.jsx)("div",{className:"name",children:a.name}),(0,n.jsx)("div",{className:"shortInfo",children:(0,n.jsx)(t.U,{children:f})})]})]}),(0,n.jsxs)("div",{className:"link",children:[(0,n.jsxs)("div",{children:[(0,n.jsx)(r.Z,{className:"icon"}),a.location]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(v.Z,{className:"icon"}),(0,n.jsx)("a",{href:a.github,children:"Github"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(m.Z,{className:"icon"}),(0,n.jsx)("a",{href:a==null?void 0:a.twitter,children:"Twitter"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(x.Z,{className:"icon"}),(0,n.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(u.Z,{className:"icon"}),(0,n.jsx)("a",{href:a==null?void 0:a.googleScholar,children:"Google Scholar"})]})]})]})}},66920:function(o,l,s){s.r(l),s.d(l,{default:function(){return e}});var h=s(62435),d="",c=s(65273),t=s(29671),i=s(86074),e=function(){return(0,i.jsxs)("div",{className:"page",children:[(0,i.jsx)(c.Z,{}),(0,i.jsxs)("div",{className:"content",children:[(0,i.jsx)("div",{className:"title",children:"Awards"}),(0,i.jsx)("div",{className:"markdown",children:(0,i.jsx)(t.U,{children:d})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/shuaijiumei","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","publications","services","cv"]}')}}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[317],{65273:function(l,a,n){n.d(a,{Z:function(){return g}});var e=n(62435),o=n(46837),c=n(8250),d=n(29671),r=n(20640),t=n.n(r),h=n(33862),v=n(1210),u=n(85093),m=n(10524),x=n(14079),i=n(49721),j=n.p+"static/logo.4700deb7.png",f=`Mphil student\r
+School of Science & Engineering\r
+The Chinese University of Hong Kong (Shenzhen)\r
+\r
+Email:zhouruixingzhu@link.cuhk.edu.cn\r
+`,s=n(86074),g=function(){var N=function(Z){Z.preventDefault(),t()(i.email),o.ZP.success("Email copied to clipboard",1)};return(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(c.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:j}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:i.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(d.U,{children:f})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(h.Z,{className:"icon"}),i.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(v.Z,{className:"icon"}),(0,s.jsx)("a",{href:i.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(u.Z,{className:"icon"}),(0,s.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(m.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),(0,s.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},66920:function(l,a,n){n.r(a),n.d(a,{default:function(){return t}});var e=n(62435),o=`# \u5927\u5BB6\u597D\uFF0C\u8FD9\u662F\u6D4B\u8BD5\u7248\u7684Markdown\u6587\u6863\r
+\r
+\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002\r
+\r
+## \u5C0F\u6807\u9898 1\r
+\r
+- \u5217\u8868\u98791\r
+- \u5217\u8868\u98792\r
+- \u5217\u8868\u98793\r
+\r
+### \u5C0F\u5C0F\u6807\u9898 1\r
+\r
+\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002\r
+\r
+#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
+\r
+1. \u6709\u5E8F\u5217\u8868\u98791\r
+2. \u6709\u5E8F\u5217\u8868\u98792\r
+3. \u6709\u5E8F\u5217\u8868\u98793\r
+\r
+##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
+\r
+> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002\r
+\r
+###### \u6700\u5C0F\u6807\u9898\r
+\r
+\`\`\`python\r
+# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801\r
+print("Hello, Markdown!")\r
+`,c=n(65273),d=n(29671),r=n(86074),t=function(){return(0,r.jsxs)("div",{className:"page",children:[(0,r.jsx)(c.Z,{}),(0,r.jsxs)("div",{className:"content",children:[(0,r.jsx)("div",{className:"title",children:"Awards"}),(0,r.jsx)("div",{className:"markdown",children:(0,r.jsx)(d.U,{children:o})})]})]})}},49721:function(l){l.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/shuaijiumei","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","publications","services","cv"]}')}}]);
