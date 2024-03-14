f13634452913a09f9c468a3cab9c28e295f3659e
Zhuzrx <zhouruixingzhu@link.cuhk.edu.cn>
2024-03-14 19:03:27 +0800

    Updates

--- p__AboutMe__index.async.js
+++ p__AboutMe__index.async.js
@@ -1,9 +1,33 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{65273:function(o,i,s){s.d(i,{Z:function(){return j}});var d=s(62435),r=s(65275),t=s(36029),c=s(29671),a=s(20640),l=s.n(a),h=s(33862),u=s(1210),v=s(85093),m=s(10524),g=s(14079),e=s(49721),x=s.p+"static/logo.4700deb7.png",f=`Mphil student in [CIE](https://sse-mphil-phd.cuhk.edu.cn/program/CIE)\r
-`,n=s(86074),j=function(){var p=function(N){N.preventDefault(),l()(e.email),r.ZP.success("Email copied to clipboard",1)};return(0,n.jsxs)("div",{className:"sidebar",children:[(0,n.jsxs)("div",{className:"profile",children:[(0,n.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:x}),(0,n.jsxs)("div",{className:"info",children:[(0,n.jsx)("div",{className:"name",children:e.name}),(0,n.jsx)("div",{className:"shortInfo",children:(0,n.jsx)(c.U,{children:f})})]})]}),(0,n.jsxs)("div",{className:"link",children:[(0,n.jsxs)("div",{children:[(0,n.jsx)(h.Z,{className:"icon"}),e.location]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(u.Z,{className:"icon"}),(0,n.jsx)("a",{href:e.github,children:"Github"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(v.Z,{className:"icon"}),(0,n.jsx)("a",{href:e==null?void 0:e.twitter,children:"Twitter"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(m.Z,{className:"icon"}),(0,n.jsx)("a",{onClick:p,href:"",children:"Email"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(g.Z,{className:"icon"}),(0,n.jsx)("a",{href:e==null?void 0:e.googleScholar,children:"Google Scholar"})]})]})]})}},92041:function(o,i,s){s.r(i),s.d(i,{default:function(){return l}});var d=s(62435),r=`\r
-Zhouruixing Zhu is currently a 2nd-year graduate student in the [School of Science and Engineering](https://sse.cuhk.edu.cn/) at [The Chinese University of Hong Kong, Shenzhen](https://www.cuhk.edu.cn/) (CUHKSZ). Her supervisor is [Prof. Pinjia He](https://pinjiahe.github.io/). She holds a Bachelor's degree from the College of Computer Science and Technology, Nanjing University of Aeronautics and Astronautics (NUAA) in 2020.\r
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{65273:function(o,a,n){n.d(a,{Z:function(){return S}});var d=n(62435),s=n(65275),t=n(36029),l=n(29671),r=n(20640),c=n.n(r),h=n(33862),u=n(1210),g=n(85093),v=n(10524),f=n(14079),i=n(49721),m=n.p+"static/logo.4700deb7.png",x=`Mphil student at CUHKSZ.\r
+`,e=n(86074),S=function(){var E=function(j){j.preventDefault(),c()(i.email),s.ZP.success("Email copied to clipboard",1)};return(0,e.jsxs)("div",{className:"sidebar",children:[(0,e.jsxs)("div",{className:"profile",children:[(0,e.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:m}),(0,e.jsxs)("div",{className:"info",children:[(0,e.jsx)("div",{className:"name",children:i.name}),(0,e.jsx)("div",{className:"shortInfo",children:(0,e.jsx)(l.U,{children:x})})]})]}),(0,e.jsxs)("div",{className:"link",children:[(0,e.jsxs)("div",{children:[(0,e.jsx)(h.Z,{className:"icon"}),i.location]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(u.Z,{className:"icon"}),(0,e.jsx)("a",{href:i.github,children:"Github"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(g.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(v.Z,{className:"icon"}),(0,e.jsx)("a",{onClick:E,href:"",children:"Email"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(f.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},92041:function(o,a,n){n.r(a),n.d(a,{default:function(){return c}});var d=n(62435),s=`\r
+\u{1F4DA}Bio: Zhouruixing Zhu is currently a 2nd-year graduate student in the [School of Science and Engineering](https://sse.cuhk.edu.cn/) at [The Chinese University of Hong Kong, Shenzhen](https://www.cuhk.edu.cn/) (CUHKSZ), advised by [Prof. Pinjia He](https://pinjiahe.github.io/). She holds a Bachelor's degree from the College of Computer Science and Technology, Nanjing University of Aeronautics and Astronautics (NUAA) in 2020.\r
 \r
-Her research interest is Large Language Model driven Software Engineering. She currently focus on Log Analysis, Root Cause Analysis, and other AI4SE topics.\r
+\u{1F4A1}Her research interest is Large Language Model driven Software Engineering. She currently focus on Log Analysis, Root Cause Analysis, and other AI4SE topics.\r
+\r
 \r
 # News\r
-- [2024/03/10] Our paper *"SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions"* was accepted by **ICDE'24** Research Track.\r
-- [2023/07/28] Our paper *["Hue: A User-Adaptive Parser for Hybrid Logs"](https://dl.acm.org/doi/abs/10.1145/3611643.3616260)* was accepted by **ESEC/FSE'23** Research Track.`,t=s(65273),c=s(29671),a=s(86074),l=function(){return(0,a.jsxs)("div",{className:"page",children:[(0,a.jsx)(t.Z,{}),(0,a.jsxs)("div",{className:"content",children:[(0,a.jsx)("div",{className:"title",children:"About"}),(0,a.jsx)("div",{className:"markdown",children:(0,a.jsx)(c.U,{children:r})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/Zhuzrx","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","publications","services","cv"]}')}}]);
+- [2024/03/10] Our paper *"SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions"* was accepted by **ICDE'24**.\r
+- [2023/07/28] Our paper *["Hue: A User-Adaptive Parser for Hybrid Logs"](https://dl.acm.org/doi/abs/10.1145/3611643.3616260)* was accepted by **ESEC/FSE'23**.\r
+\r
+\r
+# Publications\r
+### SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions\r
+Cheryl Lee, **Zhouruixing Zhu**, Tianyi Yang, Yuxin Su, Pinjia He, Michael R Lyu\r
+*ICDE'24: Proceedings of IEEE 40th International Conference on Data Engineering*\r
+\r
+### Hue: A user-adaptive parser for hybrid logs\r
+Junjielong Xu, Qiuai Fu, **Zhouruixing Zhu**, Yutong Cheng, Zhijing Li, Yuchi Ma, Pinjia He*\r
+*ESEC/FSE'23: Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering*\r
+(* Corresponding author)\r
+\r
+\r
+# Services\r
+### 2024\r
+- External Reviewer \u2013 46th International Conference on Software Engineering (ICSE 2024)\r
+- External Reviewer \u2013 33rd ACM International Conference on the Foundations of Software Engineering (FSE 2024)\r
+\r
+### 2023\r
+- External Reviewer \u2013 32nd ACM Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering (ESEC/FSE 2023)\r
+- External Reviewer \u2013 34th IEEE International Symposium on Software Reliability Engineering (ISSRE 2023)\r
+\r
+`,t=n(65273),l=n(29671),r=n(86074),c=function(){return(0,r.jsxs)("div",{className:"page",children:[(0,r.jsx)(t.Z,{}),(0,r.jsxs)("div",{className:"content",children:[(0,r.jsx)("div",{className:"title",children:"About"}),(0,r.jsx)("div",{className:"markdown",children:(0,r.jsx)(l.U,{children:s})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/Zhuzrx","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","cv"]}')}}]);
