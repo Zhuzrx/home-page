5c975aa825b2d19867b691fd5b623d3585a1a757
Zhuzrx <zhouruixingzhu@link.cuhk.edu.cn>
2024-06-17 20:42:19 +0800

    Updates

--- p__AboutMe__index.async.js
+++ p__AboutMe__index.async.js
@@ -1,30 +1,40 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{65273:function(o,a,n){n.d(a,{Z:function(){return S}});var d=n(62435),s=n(65275),t=n(36029),l=n(29671),r=n(20640),c=n.n(r),h=n(33862),u=n(1210),g=n(85093),v=n(10524),f=n(14079),i=n(49721),m=n.p+"static/logo.4700deb7.png",x=`Mphil student at CUHKSZ\r
-`,e=n(86074),S=function(){var E=function(j){j.preventDefault(),c()(i.email),s.ZP.success("Email copied to clipboard",1)};return(0,e.jsxs)("div",{className:"sidebar",children:[(0,e.jsxs)("div",{className:"profile",children:[(0,e.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:m}),(0,e.jsxs)("div",{className:"info",children:[(0,e.jsx)("div",{className:"name",children:i.name}),(0,e.jsx)("div",{className:"shortInfo",children:(0,e.jsx)(l.U,{children:x})})]})]}),(0,e.jsxs)("div",{className:"link",children:[(0,e.jsxs)("div",{children:[(0,e.jsx)(h.Z,{className:"icon"}),i.location]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(u.Z,{className:"icon"}),(0,e.jsx)("a",{href:i.github,children:"Github"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(g.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(v.Z,{className:"icon"}),(0,e.jsx)("a",{onClick:E,href:"",children:"Email"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(f.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},92041:function(o,a,n){n.r(a),n.d(a,{default:function(){return c}});var d=n(62435),s=`\r
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{65273:function(o,a,n){n.d(a,{Z:function(){return E}});var d=n(62435),s=n(65275),t=n(36029),l=n(29671),i=n(20640),c=n.n(i),h=n(33862),u=n(1210),g=n(85093),f=n(10524),v=n(14079),r=n(49721),S=n.p+"static/logo.4700deb7.png",m=`Mphil student at CUHKSZ\r
+`,e=n(86074),E=function(){var x=function(j){j.preventDefault(),c()(r.email),s.ZP.success("Email copied to clipboard",1)};return(0,e.jsxs)("div",{className:"sidebar",children:[(0,e.jsxs)("div",{className:"profile",children:[(0,e.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:S}),(0,e.jsxs)("div",{className:"info",children:[(0,e.jsx)("div",{className:"name",children:r.name}),(0,e.jsx)("div",{className:"shortInfo",children:(0,e.jsx)(l.U,{children:m})})]})]}),(0,e.jsxs)("div",{className:"link",children:[(0,e.jsxs)("div",{children:[(0,e.jsx)(h.Z,{className:"icon"}),r.location]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(u.Z,{className:"icon"}),(0,e.jsx)("a",{href:r.github,children:"Github"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(g.Z,{className:"icon"}),(0,e.jsx)("a",{href:r==null?void 0:r.twitter,children:"Twitter"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(f.Z,{className:"icon"}),(0,e.jsx)("a",{onClick:x,href:"",children:"Email"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(v.Z,{className:"icon"}),(0,e.jsx)("a",{href:r==null?void 0:r.googleScholar,children:"Google Scholar"})]})]})]})}},92041:function(o,a,n){n.r(a),n.d(a,{default:function(){return c}});var d=n(62435),s=`\r
 ## About\r
 \r
-\u{1F4DA}Bio: Zhouruixing Zhu is currently a 2nd-year graduate student in the [School of Science and Engineering](https://sse.cuhk.edu.cn/) at [The Chinese University of Hong Kong, Shenzhen](https://www.cuhk.edu.cn/) (CUHKSZ), advised by [Prof. Pinjia He](https://pinjiahe.github.io/). She holds a Bachelor's degree from the College of Computer Science and Technology, Nanjing University of Aeronautics and Astronautics (NUAA) in 2020.\r
+\u{1F4DA}Bio: Zhouruixing Zhu is currently a 2nd-year Mphil student in the [School of Science and Engineering](https://sse.cuhk.edu.cn/) at [The Chinese University of Hong Kong, Shenzhen](https://www.cuhk.edu.cn/) (CUHKSZ), advised by [Prof. Pinjia He](https://pinjiahe.github.io/). She holds a Bachelor's degree from the College of Computer Science and Technology, Nanjing University of Aeronautics and Astronautics (NUAA) in 2020.\r
 \r
 \u{1F4A1}Her research interest is Large Language Model driven Software Engineering. She currently focus on Log Analysis, Root Cause Analysis, and other AI4SE topics.\r
 \r
 \r
 ## News\r
-- [2024/03/10] Our paper *"SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions"* was accepted by **ICDE'24**.\r
+- [2024/05/27] Our paper *["HeMiRCA: Fine-Grained Root Cause Analysis for Microservices with Heterogeneous Data Sources"]* was accepted by **TOSEM**.\r
+- [2024/03/10] Our paper *["SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions"](http://arxiv.org/abs/2403.17574)* was accepted by **ICDE'24**.\r
 - [2023/07/28] Our paper *["Hue: A User-Adaptive Parser for Hybrid Logs"](https://dl.acm.org/doi/abs/10.1145/3611643.3616260)* was accepted by **ESEC/FSE'23**.\r
 \r
 \r
 ## Publications\r
+### HeMiRCA: Fine-Grained Root Cause Analysis for Microservices with Heterogeneous Data Sources\r
+**<u>Zhouruixing Zhu</u>**, Cheryl Lee, Xiaoying Tang, Pinjia He\r
+*TOSEM: ACM Transactions on Software Engineering and Methodology (Just Accepted)* 2024\r
+\r
 ### SPES: Towards Optimizing Performance-Resource Trade-Off for Serverless Functions\r
-Cheryl Lee, **Zhouruixing Zhu**, Tianyi Yang, Yuxin Su, Pinjia He, Michael R Lyu\r
-*ICDE'24: Proceedings of IEEE 40th International Conference on Data Engineering*\r
+Cheryl Lee, <u>Zhouruixing Zhu</u>, Tianyi Yang, Yuxin Su, Pinjia He, Michael R Lyu\r
+*ICDE'24: Proceedings of IEEE 40th International Conference on Data Engineering* 2024\r
 \r
 ### Hue: A user-adaptive parser for hybrid logs\r
-Junjielong Xu, Qiuai Fu, **Zhouruixing Zhu**, Yutong Cheng, Zhijing Li, Yuchi Ma, Pinjia He*\r
-*ESEC/FSE'23: Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering*\r
+Junjielong Xu, Qiuai Fu, <u>Zhouruixing Zhu</u>, Yutong Cheng, Zhijing Li, Yuchi Ma, Pinjia He*\r
+*ESEC/FSE'23: Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering* 2023\r
 (* Corresponding author)\r
 \r
 \r
 ## Services\r
+### 2025\r
+- External Reviewer \u2013 47th International Conference on Software Engineering (ICSE 2025)\r
+\r
 ### 2024\r
+- External Reviewer \u2013 39th International Conference on Automated Software Engineering 2024 (ASE 2024)\r
+- External Reviewer \u2013 33rd International Symposium on Software Testing and Analysis(ISSTA 2024)\r
 - External Reviewer \u2013 46th International Conference on Software Engineering (ICSE 2024)\r
 - External Reviewer \u2013 33rd ACM International Conference on the Foundations of Software Engineering (FSE 2024)\r
 \r
@@ -32,4 +42,4 @@ Junjielong Xu, Qiuai Fu, **Zhouruixing Zhu**, Yutong Cheng, Zhijing Li, Yuchi Ma
 - External Reviewer \u2013 32nd ACM Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering (ESEC/FSE 2023)\r
 - External Reviewer \u2013 34th IEEE International Symposium on Software Reliability Engineering (ISSRE 2023)\r
 \r
-`,t=n(65273),l=n(29671),r=n(86074),c=function(){return(0,r.jsxs)("div",{className:"page",children:[(0,r.jsx)(t.Z,{}),(0,r.jsxs)("div",{className:"content",children:[(0,r.jsx)("div",{className:"title"}),(0,r.jsx)("div",{className:"markdown",children:(0,r.jsx)(l.U,{children:s})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/Zhuzrx","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","cv"]}')}}]);
+`,t=n(65273),l=n(29671),i=n(86074),c=function(){return(0,i.jsxs)("div",{className:"page",children:[(0,i.jsx)(t.Z,{}),(0,i.jsxs)("div",{className:"content",children:[(0,i.jsx)("div",{className:"title"}),(0,i.jsx)("div",{className:"markdown",children:(0,i.jsx)(l.U,{children:s})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Zhouruixing Zhu","location":"ShenZhen, China","email":"zhouruixingzhu@link.cuhk.edu.cn","github":"https://github.com/Zhuzrx","googleScholar":"https://scholar.google.com.hk/citations?user=bKNg57IAAAAJ&hl=zh-CN","showBar":["aboutme","cv"]}')}}]);
